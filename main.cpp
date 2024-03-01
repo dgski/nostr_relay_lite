05c99cc4c2a30a7f8c4d3f6e96a5e5250478ffa5
David Gorski <gorski.dave@gmail.com>
2024-03-01 12:36:19 -0500

    More work

--- main.cpp
+++ main.cpp
@@ -7,74 +7,69 @@
 
 using Server = websocketpp::server<websocketpp::config::asio>;
 
-template<typename F>
-void forEachMatchingEvent(const std::vector<std::string>& events, const nlohmann::json& filters, F&& f) {
-  auto matchesFilter = [](const nlohmann::json& event, const nlohmann::json& filter) {
-    if (filter.contains("ids")) {
-      const auto& ids = filter["ids"];
-      if (ids.is_array() && std::find(ids.begin(), ids.end(), event["id"]) == ids.end()) {
-        bool startsWithPrefix = std::none_of(ids.begin(), ids.end(), [&event](const auto& prefix) {
-          return event["id"].get<std::string>().starts_with(prefix.template get<std::string>());
-        });
-        if (startsWithPrefix) {
-          return false;
-        }
-      }
-    }
-
-    if (filter.contains("kinds")) {
-      const auto& kinds = filter["kinds"];
-      if (kinds.is_array() && std::find(kinds.begin(), kinds.end(), event["kind"]) == kinds.end()) {
+bool matchesFilter(const nlohmann::json& event, const nlohmann::json& filter) {
+  if (filter.contains("ids")) {
+    const auto& ids = filter["ids"];
+    if (ids.is_array() && std::find(ids.begin(), ids.end(), event["id"]) == ids.end()) {
+      bool startsWithPrefix = std::none_of(ids.begin(), ids.end(), [&event](const auto& prefix) {
+        return event["id"].get<std::string>().starts_with(prefix.template get<std::string>());
+      });
+      if (startsWithPrefix) {
         return false;
       }
     }
+  }
 
-    if (filter.contains("authors")) {
-      const auto& authors = filter["authors"];
-      if (authors.is_array() && std::find(authors.begin(), authors.end(), event["pubkey"]) == authors.end()) {
-        bool startsWithPrefix = std::none_of(authors.begin(), authors.end(), [&event](const auto& prefix) {
-          return event["pubkey"].get<std::string>().starts_with(prefix.template get<std::string>());
-        });
-        if (startsWithPrefix) {
-          return false;
-        }
-      }
-    }
-
-    for (const auto& [key, value] : filter.items()) {
-      if (key[0] == '#') {
-        std::string tagName = key.substr(1);
-        const auto& values = filter["#" + tagName];
-        if (values.is_array() && std::none_of(values.begin(), values.end(), [&event, &tagName](const auto& tagValue) {
-          return event["tags"].contains({ tagName, tagValue }) && event["tags"][tagName] == tagValue;
-        })) {
-          return false;
-        }
-      }
-    }
-
-    if (filter.contains("since") && event["created_at"] < filter["since"]) {
+  if (filter.contains("kinds")) {
+    const auto& kinds = filter["kinds"];
+    if (kinds.is_array() && std::find(kinds.begin(), kinds.end(), event["kind"]) == kinds.end()) {
       return false;
     }
+  }
 
-    if (filter.contains("until") && event["created_at"] > filter["until"]) {
-      return false;
-    }
-
-    return true;
-  };
-
-  auto matchesAnyFilter = [&filters, &matchesFilter](const nlohmann::json& event) {
-    for (const auto& filter : filters) {
-      if (matchesFilter(event, filter)) {
-        return true;
+  if (filter.contains("authors")) {
+    const auto& authors = filter["authors"];
+    if (authors.is_array() && std::find(authors.begin(), authors.end(), event["pubkey"]) == authors.end()) {
+      bool startsWithPrefix = std::none_of(authors.begin(), authors.end(), [&event](const auto& prefix) {
+        return event["pubkey"].get<std::string>().starts_with(prefix.template get<std::string>());
+      });
+      if (startsWithPrefix) {
+        return false;
       }
     }
+  }
+
+  for (const auto& [key, value] : filter.items()) {
+    if (key[0] == '#') {
+      std::string tagName = key.substr(1);
+      const auto& values = filter["#" + tagName];
+      if (values.is_array() && std::none_of(values.begin(), values.end(), [&event, &tagName](const auto& tagValue) {
+        return event["tags"].contains({ tagName, tagValue }) && event["tags"][tagName] == tagValue;
+      })) {
+        return false;
+      }
+    }
+  }
+
+  if (filter.contains("since") && event["created_at"].get<int>() < filter["since"].get<int>()) {
     return false;
-  };
+  }
+
+  if (filter.contains("until") && event["created_at"].get<int>() > filter["until"].get<int>()) {
+    return false;
+  }
+
+  return true;
+};
+
+template<typename F>
+void forEachMatchingEvent(const std::vector<std::string>& events, const nlohmann::json& filters, F&& f) {
   for (const auto& event : events) {
     auto json = nlohmann::json::parse(event);
-    if (matchesAnyFilter(json)) {
+    const bool match = std::all_of(
+      filters.begin(), filters.end(),
+      [&json](const auto& filter) { return matchesFilter(json, filter); });
+    if (match) {
       f(event);
     }
   }
@@ -83,21 +78,13 @@ void forEachMatchingEvent(const std::vector<std::string>& events, const nlohmann
 class RelayToConnection {
   std::function<void(const std::string&)> _sendMessage;
   std::function<void()> _closeConnection;
-  std::vector<std::string> _events;
+  std::vector<std::string>& _events;
 public:
-  RelayToConnection(){}
-
-  void setSendMessage(const std::function<void(const std::string&)>& sendMessage) {
-    _sendMessage = sendMessage;
-  }
-
-  void setCloseConnection(const std::function<void()>& closeConnection) {
-    _closeConnection = closeConnection;
-  }
-
-  void setEvents(const std::vector<std::string>& events) {
-    _events = events;
-  }
+  RelayToConnection(
+    std::function<void(const std::string&)>&& sendMessage,
+    std::function<void()>&& closeConnection,
+    std::vector<std::string>& events
+  ) : _sendMessage(sendMessage), _closeConnection(closeConnection), _events(events) {}
 
   void handleMessage(const std::string& msg) {
     std::cout << "Message: " << msg << std::endl;
@@ -152,16 +139,16 @@ int main(int argc, const char* argv[]) {
   server.set_open_handler([&server, &connIdToRelay, &nextConnId, &events](auto connHandle) {
     std::cout << "Connection opened" << std::endl;
     const int connId = nextConnId++;
-    auto& relay = connIdToRelay[connId];
-    relay.setSendMessage([&server, connHandle](const std::string& msg) {
+    const auto sendMessage = [&server, connHandle](const std::string& msg) {
       std::cout << "Sending message: " << msg << std::endl;
       server.send(connHandle, msg, websocketpp::frame::opcode::text);
-    });
-    relay.setCloseConnection([&server, connHandle, &connIdToRelay, connId]() {
+    };
+    const auto closeConnection = [&server, connHandle, &connIdToRelay, connId]() {
       server.close(connHandle, websocketpp::close::status::normal, "Done");
       connIdToRelay.erase(connId);
-    });
-    relay.setEvents(events);
+    };
+    auto& relay = connIdToRelay.emplace(
+      connId, RelayToConnection(sendMessage, closeConnection, events)).first->second;
 
     auto connection = server.get_con_from_hdl(connHandle);
     connection->set_message_handler([&relay](auto connHandle, auto msg) {
